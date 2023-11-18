cd83512e2d630fbb28dcc36f5f0df1b24295b7bf
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-11-18 11:04:11 +0300

    html yapımızı oluşturduk

---
+++ script.js
@@ -0,0 +1,8 @@
+/* <li class="d-flex justify-content-between align-items-center bg-info px-3 rounded-2">
+    <p class="mt-3">İnputta yazılacak Değer</p>
+    <div class="d-flex gap-3">
+        <i class="fa-solid fa-thumbs-up fa-beat"></i>
+        <i class="fa-solid fa-trash fa-bounce"></i>
+    </div>
+</li> */
+
